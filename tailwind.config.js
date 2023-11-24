374d49ccec0c6a3292a18d0d6bd7e8ffc529d889
Neven Hsu <space.man0318@gmail.com>
2023-11-24 18:16:18 +0800

    config: add tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
+/** @type {import('tailwindcss/types/config').ThemeConfig} */
+const theme = require('./theme/tailwind')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
-  theme: {
-    extend: {},
-  },
+  theme,
   plugins: [require('@tailwindcss/typography')],
 }
